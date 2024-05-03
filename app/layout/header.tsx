27ddf74fc44781664924a6516bdbf5c5eb378426
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-05-03 16:10:32 +0800

    Update header.tsx

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -16,7 +16,7 @@ export default function HeaderMod(props: { contentWrapperStyle: CSSProperties, r
     const [isScrolled, setIsScrolled] = useState(false);
 
     useEffect(() => {
-        document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"KQdBh9bWzAxDECPa\",ck:\"KQdBh9bWzAxDECPa\"});</script>")
+        //document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"KQdBh9bWzAxDECPa\",ck:\"KQdBh9bWzAxDECPa\"});</script>")
 
         const handleScroll = () => {
             const currentScrollY = window.pageYOffset;
@@ -26,8 +26,14 @@ export default function HeaderMod(props: { contentWrapperStyle: CSSProperties, r
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
 
+        
     }, []);
 
+useEffect(() => {
+    eval(`!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"KQdBh9bWzAxDECPa",ck:"KQdBh9bWzAxDECPa"});`)
+    
+    },[]
+)
     const headerStyle: CSSProperties = {
         ...{
             position: 'fixed',
