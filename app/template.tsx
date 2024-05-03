052163f787575f7132117624c32323ac78b5366c
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-05-03 15:16:07 +0800

    T

--- app/template.tsx
+++ app/template.tsx
@@ -8,7 +8,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
 
         {/* 在这里插入51.la的统计代码 */}
         <Script
-            src="/sdk.51.la/js-sdk-pro.min.js?id=KQdBh9bWzAxDECPa&ck=KQdBh9bWzAxDECPa"
+            src="//sdk.51.la/js-sdk-pro.min.js?id=KQdBh9bWzAxDECPa&ck=KQdBh9bWzAxDECPa"
         />
 {/*         <script
             dangerouslySetInnerHTML={{
