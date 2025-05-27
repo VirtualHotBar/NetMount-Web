2cc06b8cae330f9d4d6a896b633698433bd93cf5
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2025-05-27 21:28:24 +0800

    unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-/*     i18n:{
-        locales:['zh','en'],
-        defaultLocale:'zh',
-    } */
-   images:{
-       remotePatterns: [
-           { protocol: 'https', hostname: 'p1.hotpe.top' },
-           { protocol: 'https', hostname: 'i.vhbs.top' }
-       ]
-   }
+    /*     i18n:{
+            locales:['zh','en'],
+            defaultLocale:'zh',
+        } */
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            { protocol: 'https', hostname: 'p1.hotpe.top' },
+            { protocol: 'https', hostname: 'i.vhbs.top' }
+        ]
+    }
 };
 
 export default nextConfig;
