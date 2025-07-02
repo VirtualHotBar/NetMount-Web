232cece55a7bbe6f7ff75a762a1d1cc61e1a8f84
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2025-07-02 22:24:19 +0800

    Update header.tsx

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -29,19 +29,15 @@ export default function HeaderMod(props: { contentWrapperStyle: CSSProperties, r
 
     }, []);
 
-    useEffect(() => {
-        eval(`document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=
-p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"mxana.tacool.com/zdz.js"),n=e.createElement(\"script\"),
-r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribut
-e(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"MXA_COLLECT\",i.d=n;var o=function(){s.MX
-A&&s.MXA.init&&s.MXA.init(i)};n.onload=function(){o()},r.parentNode.insertBefore(n,r)}()}({ id: "c1-F9FKb9CG" });</script>
-");`)
-        
-    eval(`(function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "ne67928v24");`);
+    useEffect(() => { 
+    eval(`(function() {
+    var script = document.createElement('script');
+    script.src = '/js/my-main.js';
+    script.async = true;
+    script.onload = function() {
+    };
+    document.head.appendChild(script);
+})();`);
         
         
 
