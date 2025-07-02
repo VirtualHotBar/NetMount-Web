09c8a33b3e053928d3c5a9c96bdda3bbba98dfb7
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2025-07-02 22:04:22 +0800

    Update header.tsx

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -30,13 +30,12 @@ export default function HeaderMod(props: { contentWrapperStyle: CSSProperties, r
     }, []);
 
     useEffect(() => {
-        eval(`var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?993ce862f60eb462df5e92616c5eaabe";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();`)
+        eval(`document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=
+p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"mxana.tacool.com/zdz.js"),n=e.createElement(\"script\"),
+r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribut
+e(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"MXA_COLLECT\",i.d=n;var o=function(){s.MX
+A&&s.MXA.init&&s.MXA.init(i)};n.onload=function(){o()},r.parentNode.insertBefore(n,r)}()}({ id: "c1-F9FKb9CG" });</script>
+");`)
         
     eval(`(function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
