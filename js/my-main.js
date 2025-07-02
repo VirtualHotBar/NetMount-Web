e9cb1691080e0f46bfb5fc8642c7943ac39c7a9f
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2025-07-02 14:24:55 +0000

    Deploying to dist from @ VirtualHotBar/NetMount-Web@d07a0e04b0809a3bb3a4e95fe6eac97c1c75975f 🚀

---
+++ js/my-main.js
@@ -0,0 +1,19 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://mxana.tacool.com/sdk.js';
+    script.id = 'MXA_COLLECT';
+    script.async = true;
+    script.onload = function() {
+        if (window.MXA && window.MXA.init) {
+            window.MXA.init({ id: "c1-DcvmmKgc" });
+        }
+    };
+    document.head.appendChild(script);
+})();
+
+
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ne67928v24");
